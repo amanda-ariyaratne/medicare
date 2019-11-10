d55a4c7639898d8507c49064d82c08584dd74cf9
Amanda Ariyaratne <aariyaratne@gmail.com>
2019-11-10 07:39:06 +0530

    403 error

--- app/views/restricted/index.php
+++ app/views/restricted/index.php
@@ -1,4 +1,36 @@
-<?php $this->setSiteTitle('Access Restricted'); ?>
-<?php $this->start('body'); ?>
-<h1 class="text-center red">You do not have permission to access this page.</h1>
-<?php $this->end(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="<?=PROOT?>images/favicon.ico">
+
+    <title>Medicare</title>
+    <link rel="stylesheet" type="text/css" href="<?= PROOT ?>css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= PROOT ?>css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= PROOT ?>css/style.css">
+    <!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.min.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+    <div class="main-wrapper error-wrapper">
+        <div class="error-box">
+            <h1>403</h1>
+            <h3><i class="fa fa-warning"></i> Oops! Access is denied.</h3>
+            <p>You do not have permission to access this content.</p>
+            <a href="<?= PROOT ?>" class="btn btn-primary go-home">Go to Home</a>
+        </div>
+    </div>
+    <script src="<?= PROOT ?>js/jquery-3.2.1.min.js"></script>
+	<script src="<?= PROOT ?>js/popper.min.js"></script>
+    <script src="<?= PROOT ?>js/bootstrap.min.js"></script>
+</body>
+
+</html>
